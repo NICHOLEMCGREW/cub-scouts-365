a821a07800c634c2d2f89673a786cbb92db546d6
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-05 11:25:42 -0600

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { FaFacebook, FaEnvelope } from 'react-icons/fa';
 import bsaImage from '../assets/imgs/bsa.png';
-import { Link } from 'react-router-dom'; // Assuming you are using React Router for navigation
+import { Link as ScrollLink } from 'react-scroll';
 
 const Footer = () => {
   return (
-    <footer className="min-h-100px py-10 flex justify-center items-center flex-col border-t-2 border-yellow-400 bg-[#003f87]">
-      <ul className="flex items-center mb-4">
+    <footer className="min-h-100px py-10 flex flex-col items-center justify-center border-t-2 border-[#ffb225] bg-[#003f87] text-center md:text-left">
+      <ul className="flex items-center mb-4 space-x-4">
         {/* Social Icons */}
-        <li className="m-2">
+        <li>
           <a href="https://www.facebook.com/cubscoutpack365yukon?mibextid=dGKdO6" target="_blank" rel="noopener noreferrer" className="text-white text-3xl transition duration-500 hover:transform hover:-translate-y-2">
             <FaFacebook />
           </a>
         </li>
-        <li className="m-2">
-          <a href="#" className="text-white text-3xl transition duration-500 hover:transform hover:-translate-y-2">
+        <li>
+          <a href="https://www.scouting.org/" target="_blank" rel="noopener noreferrer" className="text-white text-3xl transition duration-500 hover:transform hover:-translate-y-2">
             <img src={bsaImage} alt="BSA" className="w-8 h-8 object-contain" />
           </a>
         </li>
-        <li className="m-2">
+        <li>
           <a href="mailto:pack365yukon@gmail.com" className="text-white text-3xl transition duration-500 hover:transform hover:-translate-y-2">
             <FaEnvelope />
           </a>
@@ -26,30 +26,26 @@ const Footer = () => {
       </ul>
 
       {/* Navigation Links */}
-      <ul className="flex items-center">
-        <li className="m-2 text-white text-lg hover:text-yellow-400 transition duration-500">
-          <Link to="/">Home</Link>
+      <ul className="flex items-center space-x-4">
+        <li className="text-xl my-2 md:my-0">
+          <ScrollLink to="home" smooth="true" duration={500} className="text-white hover:text-[#ffb225] duration-500 cursor-pointer">Home</ScrollLink>
         </li>
-        <li className="m-2 text-white text-lg hover:text-yellow-400 transition duration-500">
-          <Link to="/about">About</Link>
+        <li className="text-xl my-2 md:my-0">
+          <ScrollLink to="about" smooth="true" duration={500} className="text-white hover:text-[#ffb225] duration-500 cursor-pointer">About</ScrollLink>
         </li>
-        <li className="m-2 text-white text-lg hover:text-yellow-400 transition duration-500">
-          <Link to="/events">Events</Link>
+        <li className="text-xl my-2 md:my-0">
+          <ScrollLink to="events" smooth="true" duration={500} className="text-white hover:text-[#ffb225] duration-500 cursor-pointer">Events</ScrollLink>
         </li>
-        <li className="m-2 text-white text-lg hover:text-yellow-400 transition duration-500">
-          <Link to="/contact">Contact</Link>
-        </li>
-        {/* Privacy Policy Link */}
-        <li className="m-2 text-white text-lg hover:text-yellow-400 transition duration-500">
-          <Link to="/privacy-policy">Privacy Policy</Link>
+        <li className="text-xl my-2 md:my-0">
+          <ScrollLink to="contact" smooth="true" duration={500} className="text-white hover:text-[#ffb225] duration-500 cursor-pointer">Contact</ScrollLink>
         </li>
       </ul>
 
       {/* Additional HTML */}
-      <hr className="border-white my-4" />
+      <hr className="border-white my-4 w-full md:w-auto" />
 
       {/* Copyright Section */}
-      <div className="copyright text-left text-white">
+      <div className="copyright text-white">
         <p>&copy; 2024 Pack 365. All rights reserved | Site designed and maintained by <a href="https://nicholemcgrew.netlify.app" target="_blank" className="text-blue-500">Nichole McGrew.</a></p>
       </div>
     </footer>
